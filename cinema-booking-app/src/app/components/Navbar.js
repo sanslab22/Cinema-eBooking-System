a95c712a831b9e80b8d2e9da7a3d0d11039d46f0
nivedhanatarajan7 <129971342+nivedhanatarajan7@users.noreply.github.com>
2025-10-10 21:03:39 -0400

    Make Nav buttons for login and register

--- cinema-booking-app/src/app/components/Navbar.js
+++ cinema-booking-app/src/app/components/Navbar.js
@@ -1,9 +1,21 @@
+import { Button } from "@mui/material";
 import "./Navbar.css";
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <h1>Cinema E-Booking App</h1>  
+            <div className="nav-buttons">
+                <p>
+                    Register
+                </p>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                >
+                    Login
+                </Button>
+            </div>
         </div>
     );
 };
