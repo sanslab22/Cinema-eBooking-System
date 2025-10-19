bfb53acf6a3bea1e2772d78ce6fa08bdb2258bf5
nivedhanatarajan7 <129971342+nivedhanatarajan7@users.noreply.github.com>
2025-10-19 17:46:23 -0400

    create pages for orders and profile

--- cinema-booking-app/src/app/components/Navbar.js
+++ cinema-booking-app/src/app/components/Navbar.js
@@ -1,31 +1,43 @@
-'use client'
+"use client";
 import { Button } from "@mui/material";
 import "./Navbar.css";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const Navbar = () => {
-    const router = useRouter();
+  const router = useRouter();
+  const notLoggedIn  = false;
 
-    return (
-        <div className="navbar">
-            <h1>Cinema E-Booking App</h1>  
-            <div className="nav-buttons">
-                <Link href="/createaccount" style={{textDecoration:"none"}}>
-                <p className="link">
-                    Register
-                </p>
-                </Link>
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={() => router.push("/login")}
-                >
-                    Login
-                </Button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="navbar">
+      <Link href="/" style={{ textDecoration: "none", color: "white" }}>
+        <h1>Cinema E-Booking App</h1>{" "}
+
+      </Link>
+
+      {notLoggedIn?
+      <div className="nav-buttons">
+        <Link href="/createaccount" style={{ textDecoration: "none" }}>
+          <p className="link">Register</p>
+        </Link>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => router.push("/login")}
+        >
+          Login
+        </Button>
+      </div>
+      :<div className="nav-buttons-loggedin">
+        <p>Orders</p>
+        <p>Profile</p>
+        <Button variant="contained"
+          color="secondary"
+          onClick={() => router.push("/login")}>Logout</Button>
+        </div>}
+      
+    </div>
+  );
 };
 
 export default Navbar;
